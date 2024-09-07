80a0513001ae14e5fd50c11b73f17472b9c72ee3
An <18481195+AnTheMaker@users.noreply.github.com>
2024-09-07 16:56:50 +0200

    update package name, versions

--- provider.go
+++ provider.go
@@ -1,7 +1,6 @@
-// Package libdnstemplate implements a DNS record management client compatible
-// with the libdns interfaces for <PROVIDER NAME>. TODO: This package is a
-// template only. Customize all godocs for actual implementation.
-package libdnstemplate
+// Package nanelo implements a DNS record management client compatible
+// with the libdns interfaces for Nanelo
+package nanelo
 
 import (
 	"context"
